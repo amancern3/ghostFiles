179702a189ba301ca426020526814e06d56763b0
amancern3 <amandeep842.as@gmail.com>
2022-07-31 23:11:36 -0700

    adding user auth -> html needs auth status

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ console.log("hello world");
 const express = require('express')
 const app = express()
 
+require('dotenv').config()
 const {readFile} = require('fs').promises
-app.get('/', async (request, response) => {
 
-    response.send(await readFile('./home.html', 'utf-8'))
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+app.use(auth(config))
+
+app.get('/', async (req, res) => {
+    res.send(await readFile('./home.html', 'utf-8'))
+    //res.send(authStatus(req,res))
 })
+
+function authStatus(req, res) {
+    var rtn = res.send(req.oidc.isAuthenticated()? 'Logged In' : 'Logged Out')
+    return rtn
+}
 
 app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'))
